d6b47c906531e3b8d1ceb6999c17ed29de482c63
2NNS-V <msy3824@knu.ac.kr>
2024-06-04 20:28:14 +0900

    feat(montyhall):add monty_hall method

---
+++ MontyHall.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+doors = ['Car', 'Goat 1', 'Goat 2']
+goats = ['Goat 1', 'Goat 2']
+
+def monty_hall():
+  # car, goat1, goat2 중에서 무작위로 하나의 요소 선택 (인자로 리스트만 줬음 = 디폴트로 1개만 추출)
+  original = np.random.choice(doors)
+
+  # 선택한 문이 염소라면
+  if is_goat(original):
+    # [염소, 나머지 염소, 자동차]
+    return [original, other_one(original, goats),'Car']
+
+  else:
+    # goat1, goat2 중에서 무작위로 하나의 염소 선택
+    throw_out = np.random.choice(goats)
+  return [original, throw_out, other_one(throw_out, goats)]
